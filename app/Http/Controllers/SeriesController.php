c30ba7ae93e5aab921be6ce702c78c627d62c1bf
Raiany <raianyrufino12@gmail.com>
2020-01-12 13:30:27 -0300

    changes in view

---
+++ app/Http/Controllers/SeriesController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+
+class SeriesController extends Controller
+{
+    public function index() {
+        $series = [
+            'Grey\'s Anatomy',
+            'Lost',
+            'Agents of SHIELD'
+        ];
+
+        return view ('series.index', [
+            'series' => $series
+        ]);
+
+    }
+
+}
