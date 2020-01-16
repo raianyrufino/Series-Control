f781d1f0d61b447d579445fa4f4a82d7a496f598
Raiany <raianyrufino12@gmail.com>
2020-01-16 09:19:11 -0300

    create delete method

--- database/migrations/2020_01_13_131222_create_table_series.php
+++ database/migrations/2020_01_13_131222_create_table_series.php
@@ -1,10 +1,10 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
-class CreateTableSeries extends Migration
+class CriarTabelaSeries extends Migration
 {
     /**
      * Run the migrations.
@@ -14,6 +14,7 @@ class CreateTableSeries extends Migration
     public function up()
     {
         Schema::create('series', function (Blueprint $table) {
+            $table->increments('id');
             $table->string('nome');
         });
     }
@@ -25,6 +26,6 @@ class CreateTableSeries extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('series');
+        Schema::drop('series');
     }
 }
